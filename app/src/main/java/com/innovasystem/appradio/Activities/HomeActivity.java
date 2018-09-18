3cf67f86a3bc9c65319e380471a6c3c29110a902
jjcrowHawk <jjcrow@espol.edu.ec>
2018-09-18 00:02:06 -0500

    Creacion de fragmentos para el menu de emisora y creacion de contenedores y botones para la ventana de seleccion de segmentos [#159611251]

--- app/src/main/java/com/innovasystem/appradio/Activities/HomeActivity.java
+++ app/src/main/java/com/innovasystem/appradio/Activities/HomeActivity.java
@@ -3,32 +3,48 @@ package com.innovasystem.appradio.Activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.innovasystem.appradio.Fragments.ChatFragment;
+import com.innovasystem.appradio.Fragments.FavoritosFragment;
+import com.innovasystem.appradio.Fragments.HomeFragment;
+import com.innovasystem.appradio.Fragments.NoticiasFragment;
+import com.innovasystem.appradio.Fragments.SegmentosFragment;
 import com.innovasystem.appradio.R;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment contentFragment;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    contentFragment= new HomeFragment();
+                    HomeActivity.this.changeFragment(contentFragment,R.id.frame_container_home);
                     return true;
                 case R.id.navigation_segmentos:
+                    contentFragment= new SegmentosFragment();
+                    HomeActivity.this.changeFragment(contentFragment,R.id.frame_container_home);
                     return true;
                 case R.id.navigation_favoritos:
+                    contentFragment= new FavoritosFragment();
+                    HomeActivity.this.changeFragment(contentFragment,R.id.frame_container_home);
                     return true;
                 case R.id.navigation_chat:
+                    contentFragment= new ChatFragment();
+                    HomeActivity.this.changeFragment(contentFragment,R.id.frame_container_home);
                     return true;
                 case R.id.navigation_noticias:
+                    contentFragment= new NoticiasFragment();
+                    HomeActivity.this.changeFragment(contentFragment,R.id.frame_container_home);
                     return true;
                 /*case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
@@ -55,10 +71,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+    }
+
+    public void changeFragment(Fragment fragment, int viewResource) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(viewResource, fragment);
+        ft.commit();
     }
 
 }
